c556ced08875286cf77d80bf40a920577cd1d994
Mario Peñate Fariñas <ckwanted@gmail.com>
2018-03-06 15:48:41 +0000

    Set up index.html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
 
-      </div>
-    );
-  }
+    render() {
+        return (
+          <div>
+
+          </div>
+        );
+    }
 }
 
 export default App;
