e53a5e1f7df628f8258c69508502fd8454edb8f5
Mario Peñate Fariñas <ckwanted@gmail.com>
2018-03-08 22:36:41 +0000

    Add sticky header

--- src/containers/MasterTemplate.js
+++ src/containers/MasterTemplate.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 
-import { OffCanvas, OffCanvasMenu, OffCanvasBody } from 'react-offcanvas'
-
 import {
     Header,
     Navigation,
@@ -12,14 +10,6 @@ class MasterTemplate extends Component {
 
     constructor(props) {
         super(props)
-
-        this.state = {
-            isMenuOpened: false
-        }
-    }
-
-    handleClick() {
-        this.setState({ isMenuOpened: !this.state.isMenuOpened });
     }
 
     render() {
@@ -27,29 +17,14 @@ class MasterTemplate extends Component {
         return (
             <div>
 
-                <OffCanvas width={300} transitionDuration={300} isMenuOpened={this.state.isMenuOpened} position={"left"}>
-                    <OffCanvasBody className={{}}>
-
-                        <div>
-                            <Header />
-                            <Navigation />
-                        </div>
-                        {this.props.children}
-                        <Footer />
-
-                    </OffCanvasBody>
-                    <OffCanvasMenu className={{}}>
-                        <p>Placeholder content.</p>
-                        <ul>
-                            <li>Link 1</li>
-                            <li>Link 2</li>
-                            <li>Link 3</li>
-                            <li>Link 4</li>
-                            <li>Link 5</li>
-                            <li><a href="#" onClick={this.handleClick.bind(this)}>Toggle Menu</a></li>
-                        </ul>
-                    </OffCanvasMenu>
-                </OffCanvas>
+                <div className="sticky-header">
+                    <Header />
+                    <Navigation />
+                </div>
+                <div className="sticky-header-margin">
+                    {this.props.children}
+                    <Footer />
+                </div>
 
             </div>
         )
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,4 +1,3 @@
-import MasterTemplate from './MasterTemplate'
 import Home from './Home'
 
 export {Home}
