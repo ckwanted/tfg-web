ab0d0a36930842e79b3acb15e6db204f6c1d731f
Mario Peñate Fariñas <ckwanted@gmail.com>
2018-05-06 18:18:31 +0100

    Remove react accesible accordion

--- src/containers/CourseDetail.js
+++ src/containers/CourseDetail.js
@@ -6,14 +6,6 @@ import {connect} from 'react-redux'
 import * as actionCreators from '../actions'
 
 import {
-    Accordion,
-    AccordionItem,
-    AccordionItemTitle,
-    AccordionItemBody,
-} from 'react-accessible-accordion'
-import 'react-accessible-accordion/dist/fancy-example.css'
-
-import {
 
 } from '../components'
 
@@ -32,26 +24,6 @@ class CourseDetail extends Component {
                         <div className="col-md-12">
 
                             <h2>{name}</h2>
-
-                            <Accordion>
-                                <AccordionItem>
-                                    <AccordionItemTitle>
-                                        <h3>Simple title</h3>
-                                    </AccordionItemTitle>
-                                    <AccordionItemBody>
-                                        <p>Body content</p>
-                                    </AccordionItemBody>
-                                </AccordionItem>
-                                <AccordionItem>
-                                    <AccordionItemTitle>
-                                        <h3>Complex title</h3>
-                                        <div>With a bit of description</div>
-                                    </AccordionItemTitle>
-                                    <AccordionItemBody>
-                                        <p>Body content</p>
-                                    </AccordionItemBody>
-                                </AccordionItem>
-                            </Accordion>
 
                         </div>
 
